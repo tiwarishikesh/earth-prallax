963c3e5c8d5f3a648d0e126ee8fc0e238b24920c
Rishi <rishi@kanris.biz>
2022-11-14 21:42:36 +0530

    Initial commit

--- ReadMe.md
+++ ReadMe.md
@@ -1,17 +1,46 @@
-Parallax Earth Animation
+Nike Landing Page
 ========
 
-This is me playing around with a parllax idea I came across on Instagram from an account by the name of [ui.alok](https://www.instagram.com/ux.alok/). 
+This is me playing around with a idea I came across on Instagram from an account by the name of [ui.alok](https://www.instagram.com/ux.alok/). 
 
 
-[Original Figma Post](https://www.instagram.com/p/CjNiBtDhKwz/)
+[Original Figma Post](https://www.instagram.com/p/CkGCBzygrIH/)
 
-[Live Demo](https://kanr.is/pens/earth-parallax/)
+[Live Demo](https://kanr.is/pens/nike-landing/)
 
 Explanation for beginners
 --------
 
 This was created using GreenSock GSAP, ScrollTrigger and Timeline
+
+
+
+```
+$(".ipad").css('top',$(window).height()*0.5 - $(".ipad").height()*0.5);
+$(".ipad").css('left',$(window).width()*0.5 - $(".ipad").width()*0.5);
+```
+Center the div containe iPad and overlay
+
+
+```
+let zoomRatio = Math.max($(window).width() / $(".ipad").width(), $(window).height() / $(".ipad").height()) * 1.1;
+```
+Choose the greater of the ratios so that the screen is covered both by width adn height.
+1.1x is done so that the border of iPad overflows as well
+
+
+```
+$('.with-whitelogo').css('top',$('.ipad').offset().top + $('.ipad').height() - $('.with-whitelogo').offset().top);
+```
+Place the section right below the iPad div bottom
+
+
+```
+var distance = $('.startLogo').offset().top - $('.altLogo').closest('.row').offset().top;
+$(".altLogo").css('transform',`translate3d(${horizontalOffset}px, ${distance}px, 0px)`);
+```
+Reveal the logo as the section moves upward
+
 
 ```
 const tl = gsap.timeline({
@@ -24,11 +53,10 @@ const tl = gsap.timeline({
         }
     });
     tl
-        .to('.earth', { x: $(".earth").width()*-0.4,top:'-100vh',rotation:'180deg', duration: 0.4, stagger: 0.1, ease: Power2.inOut })
-        .to('.earth', { x: $(window).width()*0.7, top:'-100vh',   rotation:'360deg', duration: 0.4, stagger: 0.1,ease: Power2.inOut } );    
+        .to('.ipad', { scale: 1, duration: 0.4, stagger: 0.1, ease: Power2.inOut });    
 ```
 
-The landing-section class division has been assigned height of 300 window heights and tmeline is broken in three equal parts thus maintaining the scroll positions
+The iPad scales down to original size
 
 
 For more information:
--- app.js
+++ app.js
@@ -7,28 +7,44 @@
         scrub: true
     }
 }); */
-
+let horizontalOffset = 0;
 $(document).ready(()=>{
-    $('.earth').css('width','300vh');
-    /* $('.earth').css('left','-250vh'); */
+    /* $('.earth').css('width','300vh');
+    $('.earth').css('left','-250vh');
     $('.earth').css('left',$(document).width()*0.5 - $(".earth").width()*0.5);
     $('.earth').css('top','55vh');
-    $('.earth').css('transform','rotate(90deg)');
+    $('.earth').css('transform','rotate(90deg)'); */
     setTimeout(() => {
+        $(".ipad").css('top',$(window).height()*0.5 - $(".ipad").height()*0.5);
+        $(".ipad").css('left',$(window).width()*0.5 - $(".ipad").width()*0.5);
+
+        let zoomRatio = Math.max($(window).width() / $(".ipad").width(), $(window).height() / $(".ipad").height()) * 1.1;
         window.scrollTo(0, document.body.scrollHeight);
+        $('.ipad').css('transform',`scale(${zoomRatio*1.1})`);
+
+
+        /* SET THE LOGO TO CENTER OF SCREEN */
+        horizontalOffset = $(window).width()*0.5 - $('svg').width()*0.5;
+        
+        TweenLite.to($('.startLogo'),0,{ x: horizontalOffset,y: $(window).height()*0.5 - $('svg').height()*0.5, force3D:true});
+        setTimeout(() => {
+            $('.with-whitelogo').css('top',$('.ipad').offset().top + $('.ipad').height() - $('.with-whitelogo').offset().top);
+        }, 250);
+
         AnimationsInit();
         setTimeout(() => {
             $(document).scrollTop(0);
+
             $(".loader").fadeOut();
         }, 1500);
-    }, 500);
+    }, 1000);
 })
 
 function AnimationsInit() {
-    gsap.to(".earth",{
+    gsap.to(".ipad",{
         scrollTrigger:{
             trigger: ".landing-section",
-            pin:".earth",
+            pin:".ipad",
             start:"top top",
             end:"bottom bottom",
             scrub: true,
@@ -45,56 +61,21 @@ function AnimationsInit() {
         }
     });
     tl
-        .to('.earth', { x: $(".earth").width()*-0.4,top:'-100vh',rotation:'180deg', duration: 0.4, stagger: 0.1, ease: Power2.inOut })
-        .to('.earth', { x: $(window).width()*0.7, top:'-100vh',   rotation:'360deg', duration: 0.4, stagger: 0.1,ease: Power2.inOut } );    
+        .to('.ipad', { scale: 1, duration: 0.4, stagger: 0.1, ease: Power2.inOut }); 
 
     const t2 = gsap.timeline({
         scrollTrigger: {
             trigger: ".landing-section",
-            start: "top top",
+            start: "-20vh top",
             end: "bottom bottom",
             scrub: true,
             toggleActions: "play reverse play reverse"
         }
     });
     t2
-        .to('.text-wrapper-one', { opacity: -0.5, duration: 0.4, stagger: 0.1 })
-        .to('.text-wrapper-one', { opacity: 0, duration: 0.4, stagger: 0.1 });
+        .to('.blackoverlay', { opacity: 1, duration: 0.4, stagger: 0.1, ease: Power2.inOut }); 
 
-    gsap.to(".text-wrapper-two",{
-        scrollTrigger:{
-            trigger: ".landing-section",
-            pin:".text-wrapper-two",
-            start:"top top",
-            end:"bottom bottom",
-            scrub: true,
-        },
-    });
-    
     const t3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".landing-section",
-            start: "-20vh top",
-            end: "bottom bottom",
-            scrub: true,
-            toggleActions: "play reverse play reverse",
-        }
-    });
-    t3
-        .to('.text-wrapper-two', { opacity: 1, duration: 0.4, stagger: 0.1 })
-        .to('.text-wrapper-two', { opacity: -0.25, duration: 0.4, stagger: 0.1 } );
-
-    gsap.to(".text-wrapper-three",{
-        scrollTrigger:{
-            trigger: ".landing-section",
-            pin:".text-wrapper-three",
-            start:"top top",
-            end:"bottom bottom",
-            scrub: true,
-        },
-    });
-    
-    const t4 = gsap.timeline({
         scrollTrigger: {
             trigger: ".landing-section",
             start: "-20vh top",
@@ -103,7 +84,48 @@ function AnimationsInit() {
             toggleActions: "play reverse play reverse"
         }
     });
-    t4
-        .to('.text-wrapper-three', { opacity: -0.25, duration: 0.4, stagger: 0.1 })
-        .to('.text-wrapper-three', { opacity: 1, duration: 0.4, stagger: 0.1 } );
+    t3
+        .to('.startLogo', { opacity: 1,delay:0.1, duration: 0.4, stagger: 0.1, ease: Power2.inOut }); 
+
+    const headerTimeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".landing-section",
+            start: "-20vh top",
+            end: "bottom bottom",
+            scrub: true,
+            toggleActions: "play reverse play reverse"
+        }
+    });
+    headerTimeline
+        .to('header', { opacity: 1,delay:0.4, duration: 0.4, stagger: 0.1, ease: Power2.inOut }); 
+
+
+    (Array.from($(".col-sm-4"))).forEach((x,i)=>{
+        gsap.to(x, {
+            opacity: 1,
+            top:0,
+            ease: 'none',
+            scrollTrigger: {
+                trigger: x,
+                start: `top bottom`,
+                end: `${50*i}px 50%`,
+                scrub: true
+            }
+        });
+    });
+
+    /* (Array.from($(".altLogo"))).forEach((x, i)=>{
+
+    }) */
 }
+
+function logoSwitch () {
+      var distance = $('.startLogo').offset().top - $('.altLogo').closest('.row').offset().top;
+       $(".altLogo").css('transform',`translate3d(${horizontalOffset}px, ${distance}px, 0px)`);
+      
+      /* TweenLite.to($('.altLogo'),0,{duration:0, y:distance, x: horizontalOffset, force3D:true}); */
+  };
+  
+  $(document).scroll(function() {logoSwitch();});
+  
+  logoSwitch();
